2e7272e5273c7191473d93f6eeb92f2ec8ebb3d8
An0 <ainine.nassim@gmail.com>
2025-04-15 18:20:11 +0200

    setting menu

--- competitive-wedding-sim/main_menu.gd
+++ competitive-wedding-sim/main_menu.gd
@@ -10,7 +10,7 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_selector.tscn")
 
 func _on_option_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/setting_menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit() # Replace with function body.
