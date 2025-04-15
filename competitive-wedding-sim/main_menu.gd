ec8572d492102fbdd7ded5bca46cb91ca606af17
An0 <ainine.nassim@gmail.com>
2025-04-15 17:53:50 +0200

    Menu Animation and other small things

--- competitive-wedding-sim/main_menu.gd
+++ competitive-wedding-sim/main_menu.gd
@@ -14,8 +14,3 @@ func _on_option_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit() # Replace with function body.
-	
-	
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept") :
-		get_tree().change_scene_to_file("res://Scenes/level_selector.tscn")
