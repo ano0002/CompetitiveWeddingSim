46a382fc9b7dfb04e06b20d101c09962ac7be0e4
Gaspard BROM <gaspard.brom@gmail.com>
2025-04-16 11:20:30 +0200

    Added obstacles and Music when starting Level1

--- competitive-wedding-sim/Scenes/Levels/Lvl1/level.gd
+++ competitive-wedding-sim/Scenes/Levels/Lvl1/level.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var timer = $GameTimer
 @onready var label = $TimerLabel
-var countdown : int = 45
+@export var countdown : int = 45
 
 func _ready():
 	timer.timeout.connect(_on_timer_timeout)
@@ -14,3 +14,5 @@ func _on_timer_timeout():
 	if countdown <= 0:
 		timer.stop()
 		label.text = "Time's up!"
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file("res://Scenes/WinLoseUI/Lose_Menu.tscn")
